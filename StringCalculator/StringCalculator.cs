11d03fe792f827ad69411fda42bea6172c997f2d
Aundre Jess <aundrejess@gmail.com>
2016-12-16 16:41:17 +0000

    Kata 1st Upload

---
+++ StringCalculator/StringCalculator.cs
@@ -0,0 +1,34 @@
+﻿namespace StringCalculatorKata
+{
+    public class StringCalculator
+    {
+        public int Add(string numbersToParse)
+        {
+            if (ThereAreNo(numbersToParse))
+            {
+                return 0;
+            }
+
+            if (numbersToParse.Contains(","))
+            {
+                var singleNumbers = numbersToParse.Split(',');
+
+                var firstNumberToParse = singleNumbers[0];
+                var secondNumberToParse = singleNumbers[1];
+
+                var firstNumber = int.Parse(firstNumberToParse);
+                var secondNumber = int.Parse(secondNumberToParse);
+
+                return firstNumber + secondNumber;
+            }
+
+            var number = int.Parse(numbersToParse);
+            return number;
+        }
+
+        private static bool ThereAreNo(string numbersToParse)
+        {
+            return string.IsNullOrEmpty(numbersToParse);
+        }
+    }
+}
---
+++ StringCalculatorTests/StringCalculatorShould.cs
@@ -0,0 +1,56 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using StringCalculatorKata;
+
+namespace StringCalculatorTests
+{
+    [TestFixture]
+    public class StringCalculatorShould
+    {
+
+        [Test]
+        public void Return0_WhenAdding_GivenNoInput()
+        {
+            var  stringCalculator = new StringCalculator();
+
+            var sum = stringCalculator.Add("");
+
+            sum.Should().Be(0);
+        }
+
+        [TestCase("1",1)]
+        [TestCase("2",2)]
+        [TestCase("10",10)]
+        public void ReturnNumber_WhenAdding_GivenSingleNumberToParse(
+            string singleNumberToParse, int expectedNumber)
+        {
+            var stringCalculator = new StringCalculator();
+
+            var number = stringCalculator.Add(singleNumberToParse);
+
+            number.Should().Be(expectedNumber);
+        }
+
+        [TestCase("1,1", 2)]
+        [TestCase("1,2", 3)]
+        [TestCase("2,2", 4)]
+        public void ReturnSum_WhenAdding_GivenTwoNumbersToParse(string numbersToParse, int expectedSum)
+        {
+            var stringCalculator = new StringCalculator();
+
+            var sum = stringCalculator.Add(numbersToParse);
+
+            sum.Should().Be(expectedSum);
+        }
+
+        //[TestCase("1,1,1", 3)]
+        //public void ReturnSum_WhenAdding_GivenManyNumbersToParse(string numbersToParse, int expectedSum)
+        //{
+        //    var stringCalculator = new StringCalculator();
+
+        //    var sum = stringCalculator.Add(numbersToParse);
+
+        //    sum.Should().Be(expectedSum);
+        //}
+    }
+}
